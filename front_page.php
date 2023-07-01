839c243d3473bdc4f614a80cc451f5ed6cea074d
kbprabowo <kbprabowo@outlook.com>
2023-07-01 22:02:30 +0800

    add: modal in services button

--- front_page.php
+++ front_page.php
@@ -42,7 +42,29 @@ Template Name: Front page
                 foreach ($slides as $slide): ?>
                 <div class="col-md-4 mb-5">
                     <div><img src="<?php echo $slide['services_img']['url']; ?>" class="services_img" /></div>
-                    <div class="btn_main"><a href="#"><?php echo $slide['services_button_label']; ?></a></div>
+                    <div class="btn_main"><a href="#" data-toggle="modal"
+                            data-target="#exampleModalCenter"><?php echo $slide['services_button_label']; ?></a></div>
+                </div>
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <?php endforeach ?>
             </div>
